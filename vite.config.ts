02f48e8cf0adb70b54b631e5604b34ba6d3850a9
Mohammad <hauwi1430@gmail.com>
2026-07-30 18:58:50 +0300

    Ready for netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/kids-damascus/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
